b3e5699c8b4f152a8ce1f3d47c0363da4e0ea425
Rachel Huch <rachelhuch@gmail.com>
2020-01-14 16:11:23 -0500

    Day 2 Exercises Complete

--- module-1/02_Variables_Data_Types/student-exercise/VariableNaming/Program.cs
+++ module-1/02_Variables_Data_Types/student-exercise/VariableNaming/Program.cs
@@ -213,144 +213,228 @@ namespace VariableNaming
             morning and 45 minutes shoveling snow on Saturday afternoon. How
             much total time did she spend shoveling snow?
             */
-
+            int mornShovel = 90;
+            int afterNoonShovel = 45;
+            int totalShovel = mornShovel + afterNoonShovel;
+            Console.WriteLine(totalShovel + " minutes");
             /*
             26. Mrs. Hilt bought 6 hot dogs. Each hot dog cost 50 cents. How much
             money did she pay for all of the hot dogs?
             */
-
+            int hiltHotDogs = 6;
+            decimal hotDogCost = .5M;
+            decimal hotDogTotal = hiltHotDogs * hotDogCost;
+            Console.WriteLine(hotDogTotal);
             /*
             27. Mrs. Hilt has 50 cents. A pencil costs 7 cents. How many pencils can
             she buy with the money she has?
             */
-
+            int hiltMoney = 50;
+            int pencilCost = 7;
+            int totalPencils = hiltMoney / pencilCost;
+            Console.WriteLine(totalPencils);
             /*
             28. Mrs. Hilt saw 33 butterflies. Some of the butterflies were red and others
             were orange. If 20 of the butterflies were orange, how many of them
             were red?
             */
-
+            int butterflyTotal = 33;
+            int orangeButterflies = 20;
+            int redButterflies = butterflyTotal - orangeButterflies;
+            Console.WriteLine(redButterflies);
             /*
             29. Kate gave the clerk $1.00. Her candy cost 54 cents. How much change
             should Kate get back?
             */
+            decimal clerkMoney = 1.00M;
+            decimal candyCost = .54M;
+            decimal kateChange = clerkMoney - candyCost;
+            Console.WriteLine(kateChange);
 
             /*
             30. Mark has 13 trees in his backyard. If he plants 12 more, how many trees
             will he have?
             */
-
+            int backTrees = 13;
+            int moreTrees = 12;
+            int totalTrees = backTrees + moreTrees;
+            Console.WriteLine(totalTrees);
             /*
             31. Joy will see her grandma in two days. How many hours until she sees
             her?
             */
-
+            int daysTilGrandma = 2;
+            const int hrsInaDay = 24;
+            int hrsTilGrandma = hrsInaDay * daysTilGrandma;
+            Console.WriteLine(hrsTilGrandma);
             /*
             32. Kim has 4 cousins. She wants to give each one 5 pieces of gum. How
             much gum will she need?
             */
-
+            const int cousins = 4;
+            int gumPerCousin = 5;
+            int totalGumNeeded = cousins * gumPerCousin;
+            Console.WriteLine(totalGumNeeded);
             /*
             33. Dan has $3.00. He bought a candy bar for $1.00. How much money is
             left?
             */
-
+            decimal dansMoney = 3.00M;
+            decimal candyBarCost = 1.00M;
+            decimal moneyLeftOver = dansMoney - candyBarCost;
+            Console.WriteLine(moneyLeftOver);
             /*
             34. 5 boats are in the lake. Each boat has 3 people. How many people are
             on boats in the lake?
             */
-
+            int boats = 5;
+            int peoplePerBoat = 3;
+            int totalPeople = boats * peoplePerBoat;
+            Console.WriteLine(totalPeople);
             /*
             35. Ellen had 380 legos, but she lost 57 of them. How many legos does she
             have now?
             */
-
+            int totalLegos = 308;
+            int lostLegos = 57;
+            int leftLegos = totalLegos - lostLegos;
+            Console.WriteLine(leftLegos);
             /*
             36. Arthur baked 35 muffins. How many more muffins does Arthur have to
             bake to have 83 muffins?
             */
-
+            int bakedMuffins = 35;
+            int muffinsNeeded = 83;
+            int muffinsLeftToBake = muffinsNeeded - bakedMuffins;
+            Console.WriteLine(muffinsLeftToBake);
             /*
             37. Willy has 1400 crayons. Lucy has 290 crayons. How many more
             crayons does Willy have then Lucy?
             */
-
+            int willyCrayons = 1400;
+            int lucyCrayons = 290;
+            int moreCrayons = willyCrayons - lucyCrayons;
+            Console.WriteLine(moreCrayons);
             /*
             38. There are 10 stickers on a page. If you have 22 pages of stickers, how
             many stickers do you have?
             */
-
+            int stickersPerPage = 10;
+            int pages = 22;
+            int totalStickers = stickersPerPage * pages;
+            Console.WriteLine(totalStickers);
             /*
             39. There are 96 cupcakes for 8 children to share. How much will each
             person get if they share the cupcakes equally?
             */
-
+            int cupCakes = 96;
+            int children = 8;
+            int cupcakesEach = cupCakes / children;
+            Console.WriteLine(cupcakesEach);
             /*
             40. She made 47 gingerbread cookies which she will distribute equally in
             tiny glass jars. If each jar is to contain six cookies each, how many
             cookies will not be placed in a jar?
             */
-
+            int gingerbreadCookies = 47;
+            int cookiesPerJar = 6;
+            int remainderCookies = gingerbreadCookies % cookiesPerJar;
+            Console.WriteLine(remainderCookies);
             /*
             41. She also prepared 59 croissants which she plans to give to her 8
             neighbors. If each neighbor received and equal number of croissants,
             how many will be left with Marian?
             */
-
+            int croissantTotal = 59;
+            int numberOfNeighbors = 8;
+            int leftOverCroissants = croissantTotal % numberOfNeighbors;
+            Console.WriteLine(leftOverCroissants);
             /*
             42. Marian also baked oatmeal cookies for her classmates. If she can
             place 12 cookies on a tray at a time, how many trays will she need to
             prepare 276 oatmeal cookies at a time?
             */
-
+            int oatmealCookiesNeeded = 276;
+            int cookiesOnSheet = 12;
+            int numberOfTrays = oatmealCookiesNeeded / cookiesOnSheet;
+            Console.WriteLine(numberOfTrays);
             /*
             43. Marian’s friends were coming over that afternoon so she made 480
             bite-sized pretzels. If one serving is equal to 12 pretzels, how many
             servings of bite-sized pretzels was Marian able to prepare?
             */
-
+            int pretzelTotal = 480;
+            int pretzelServing = 12;
+            int servingsPrepared = pretzelTotal / pretzelServing;
+            Console.WriteLine(servingsPrepared);
             /*
             44. Lastly, she baked 53 lemon cupcakes for the children living in the city
             orphanage. If two lemon cupcakes were left at home, how many
             boxes with 3 lemon cupcakes each were given away?
             */
+            int orphanageCupcakes = 53-2;
+            int cupcakesPerBox = 3;
+            int boxesTotal = orphanageCupcakes / cupcakesPerBox;
+            Console.WriteLine(boxesTotal);
 
             /*
             45. Susie's mom prepared 74 carrot sticks for breakfast. If the carrots
             were served equally to 12 people, how many carrot sticks were left
             uneaten?
             */
-
+            int carrotSticks = 74;
+            int people = 12;
+            int carrotSticksLeft = carrotSticks % people;
+            Console.WriteLine(carrotSticksLeft);
             /*
             46. Susie and her sister gathered all 98 of their teddy bears and placed
             them on the shelves in their bedroom. If every shelf can carry a
             maximum of 7 teddy bears, how many shelves will be filled?
             */
-
+            int teddyBears = 98;
+            int shelfCapacity = 7;
+            int numberOfShelves = teddyBears / shelfCapacity;
+            Console.WriteLine(numberOfShelves);
             /*
             47. Susie’s mother collected all family pictures and wanted to place all of
             them in an album. If an album can contain 20 pictures, how many
             albums will she need if there are 480 pictures?
             */
-
+            int picturesTotal = 480;
+            int picturesPerAlbum = 20;
+            int albumsNeeded = picturesTotal / picturesPerAlbum;
+            Console.WriteLine(albumsNeeded);
             /*
             48. Joe, Susie’s brother, collected all 94 trading cards scattered in his
             room and placed them in boxes. If a full box can hold a maximum of 8
             cards, how many boxes were filled and how many cards are there in
             the unfilled box?
             */
+            int tradingCards = 94;
+            int cardsPerBox = 8;
+            int boxesFilled = tradingCards / cardsPerBox;
+            int unfilledBoxCards = tradingCards % cardsPerBox;
+            Console.WriteLine(boxesFilled);
+            Console.WriteLine(unfilledBoxCards);
+
 
             /*
             49. Susie’s father repaired the bookshelves in the reading room. If he has
             210 books to be distributed equally on the 10 shelves he repaired,
             how many books will each shelf contain?
             */
-
+            int booksInReadingRoom = 210;
+            int shelves = 10;
+            int booksPerShelf = booksInReadingRoom / shelves;
+            Console.WriteLine(booksPerShelf);
             /*
             50. Cristina baked 17 croissants. If she planned to serve this equally to
             her seven guests, how many will each have?
             */
-
+            int croissantsChristina = 17;
+            int numberOfGuests = 7;
+            int howManyCroissantsEach = croissantsChristina / numberOfGuests;
+            Console.WriteLine(howManyCroissantsEach);
             /*
                 CHALLENGE PROBLEMS
             */
@@ -361,6 +445,7 @@ namespace VariableNaming
             Hint: Calculate the hourly rate for each painter, combine them, and then divide the total walls in feet by the combined hourly rate of the painters.
             Challenge: How many days will it take the pair to paint 623 rooms assuming they work 8 hours a day?.
             */
+            int feetPerRoom=12*14;
 
             /*
             Create and assign variables to hold your first name, last name, and middle initial. Using concatenation,
