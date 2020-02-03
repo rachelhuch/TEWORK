03b18759c90cbba8f719dab88b5c3b5de3a1fd66
Rachel Huch <rachelhuch@gmail.com>
2020-02-03 15:14:26 -0500

    Kata_Potter_Tests_Get_Cost_Tests_Failing

--- module-1/16_Test_Driven_Development/student-exercise/Exercises.Tests/KataPotterTests.cs
+++ module-1/16_Test_Driven_Development/student-exercise/Exercises.Tests/KataPotterTests.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools;
+
 
 namespace Exercises.Tests
 {
     [TestClass]
     public class KataPotterTests
     {
+
         
+            [DataTestMethod]
+            [DataRow(0, 0, 0, 0, 0, 0)]
+
+            public void GetCostTest(int[] books, decimal expectedCost)
+            {
+                Cart cart = new Cart();
+                decimal result = cart.GetCost(books);
+                Assert.AreEqual(result, expectedCost);
+            }
+
+
+        }
     }
-}
