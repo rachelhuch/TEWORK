c2ccdbe0bfa950663dc9a56656a9b499934f5c49
Rachel Huch <rachelhuch@gmail.com>
2020-02-04 15:19:08 -0500

    my homework update

--- module-1/17_FileIO_Reading_in/student-exercise/WordSearch/Program.cs
+++ module-1/17_FileIO_Reading_in/student-exercise/WordSearch/Program.cs
@@ -7,15 +7,71 @@ namespace WordSearch
     {
         static void Main(string[] args)
         {
+            int num = 1;
             //1. Ask the user for the search string
             Console.WriteLine("Please enter a search string.");
             string input = Console.ReadLine();
+
+            Console.WriteLine("Would you like this search to be case sensitive? y or n?");
+            string caseSensitiveInput = Console.ReadLine();
+         
+
             //2. Ask the user for the file path
+
+            //alices_adventures_in_wonderland.txt
             Console.WriteLine("Please enter the file path.");
             string stream = Console.ReadLine();
-            using (StreamReader str = new StreamReader(stream)
+
+            if (caseSensitiveInput.StartsWith('y'))
             {
 
+
+
+                using (StreamReader str = new StreamReader(stream))
+                {
+                    while (!str.EndOfStream)
+                    {
+                        string line = str.ReadLine();
+                        if (line.Contains(input))
+                        {
+
+                            Console.WriteLine(line);
+                            Console.WriteLine(num);
+                            num++;
+
+                        }
+                        else
+                        {
+                            num++;
+                        }
+
+                    }
+                }
+            }
+
+            else
+            {
+                using (StreamReader str = new StreamReader(stream))
+                {
+                    while (!str.EndOfStream)
+                    {
+                        string line = str.ReadLine();
+                        string lowerCaseLine = line.ToLower();
+                        if (lowerCaseLine.Contains(input.ToLower()))
+                        {
+
+                            Console.WriteLine(line);
+                            Console.WriteLine(num);
+                            num++;
+
+                        }
+                        else
+                        {
+                            num++;
+                        }
+
+                    }
+                }
             }
             //3. Open the file
             //4. Loop through each line in the file
@@ -23,3 +79,4 @@ namespace WordSearch
         }
     }
 }
+
