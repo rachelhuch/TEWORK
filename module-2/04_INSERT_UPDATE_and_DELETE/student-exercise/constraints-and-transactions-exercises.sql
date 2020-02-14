44c19b121572f8bb2f48a5eceed4ef5c26e3ae07
Rachel Huch <rachelhuch@gmail.com>
2020-02-13 21:13:33 -0500

    UPDATE HOMEWORK

--- module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-exercise/constraints-and-transactions-exercises.sql
@@ -2,95 +2,89 @@
 -- The following changes are applied to the "dvdstore" database.**
 
 -- 1. Add actors, Hampton Avenue, and Lisa Byway to the actor table.
-SELECT * FROM actor
-ORDER BY last_name
-
-BEGIN TRANSACTION;
 
 INSERT INTO actor(first_name,last_name)
-VALUES ('HAMPTON','AVENUE'),('LISA','BYWAY')
+VALUES ('Hampton', 'Avenue'), ('Lisa', 'Byway')
 
-
-COMMIT;
-
-ROLLBACK;
 -- 2. Add "Euclidean PI", "The epic story of Euclid as a pizza delivery boy in
 -- ancient Greece", to the film table. The movie was released in 2008 in English.
 -- Since its an epic, the run length is 3hrs and 18mins. There are no special
 -- features, the film speaks for itself, and doesn't need any gimmicks.
-SELECT * FROM film
-ORDER BY title
 
-BEGIN TRANSACTION;
 
-INSERT INTO film(title, description, release_year, language_id,length)
-VALUES ('Euclidean PI','The epic story of Euclid as a pizza delivery boy in 
--- ancient Greece','2008','1','198')
+INSERT INTO film (title, description, release_year, language_id, length)
+Values('Euclidean PI', 'The epic story of Euclid as a pizza delivery boy in
+-- ancient Greece', 2008, 1, 198)
 
-COMMIT;
 
-ROLLBACK;
 -- 3. Hampton Avenue plays Euclid, while Lisa Byway plays his slightly
 -- overprotective mother, in the film, "Euclidean PI". Add them to the film.
-Select *
+
+SELECT actor_id
 FROM actor
+WHERE first_name = 'Hampton' AND last_name = 'Avenue'
 
-BEGIN TRANSACTION;
+INSERT INTO film_actor(film_id, actor_id)
+VALUES (1001, 202), (1001, 201)
 
-INSERT INTO film_actor(actor_id, film_id)
-VALUES ((SELECT actor_id FROM ACTOR WHERE first_name = 'Hampton' AND last_name = 'Avenue'), (SELECT film_id  FROM WHERE title = 'Euclidean PI'))
-
-INSERT INTO film_actor( actor_id, film_id)
-VALUES ((SELECT actor_id FROM ACTOR WHERE first_name = 'Lisa' AND last_name = 'Byway'), (SELECT film_id FROM WHERE title = 'Euclidean PI'))
-SELECT * FROM film_actor
-ORDER BY film_id DESC
-
-COMMIT;
-
-BEGIN TRANSACTION;
-
-INSERT INTO film_actor(actor_id,film_id)
-VALUES ((SELECT actor_id FROM actor WHERE first_name = 'HAMPTON' AND last_name = 'AVENUE'),(SELECT film_id FROM film WHERE title = 'Euclidean PI'))
-
-INSERT INTO film_actor(actor_id,film_id)
-VALUES ((SELECT actor_id FROM actor WHERE first_name = 'LISA' AND last_name = 'BYWAY'),(SELECT film_id FROM film WHERE title = 'Euclidean PI'))
-
-COMMIT;
-
-ROLLBACK;
--- 4. Add Mathmagical to the category table.
+-- 4. Add Mathemagical to the category table.
 INSERT INTO category (name)
-VALUES ('Mathemagical')
+VALUES ('Mathmagical')
 
 -- 5. Assign the Mathmagical category to the following films, "Euclidean PI",
 -- "EGG IGBY", "KARATE MOON", "RANDOM GO", and "YOUNG LANGUAGE"
-INSERT INTO 
+
+INSERT INTO film_category (film_id, category_id)
+Values(996, 17), (274, 17), (1001, 17), (494, 17), (714, 17)
 
 -- 6. Mathmagical films always have a "G" rating, adjust all Mathmagical films
 -- accordingly.
 -- (5 rows affected)
+UPDATE film
+SET rating ='G'
+WHERE film_id IN (996, 274, 1001, 494, 714)
 
 -- 7. Add a copy of "Euclidean PI" to all the stores.
+
+INSERT INTO inventory(film_id, store_id)
+VALUES (1001, 1), (1001, 2)
+
+
+
+
 
 -- 8. The Feds have stepped in and have impounded all copies of the pirated film,
 -- "Euclidean PI". The film has been seized from all stores, and needs to be
 -- deleted from the film table. Delete "Euclidean PI" from the film table.
 -- (Did it succeed? Why?)
+
+DELETE from film
+WHERE title= 'Euclidean PI'
+
 -- <YOUR ANSWER HERE>
+--No because it's being used in other tables - actor and film_actor
 
 -- 9. Delete Mathmagical from the category table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- <YOUR ANSWER HERE> No because it's being referenced in other tables
+DELETE from category
+WHERE name = 'Mathmagical'
 
--- 10. Delete all links to Mathmagical in the film_category tale.
+-- 10. Delete all links to Mathmagical in the film_category table.
 -- (Did it succeed? Why?)
--- <YOUR ANSWER HERE>
+-- <YOUR ANSWER HERE> yes
+DELETE 
+FROM film_category 
+WHERE category_id =17
 
 -- 11. Retry deleting Mathmagical from the category table, followed by retrying
 -- to delete "Euclidean PI".
 -- (Did either deletes succeed? Why?)
 -- <YOUR ANSWER HERE>
+DELETE from category where name ='Mathmagical' --yes success
+DELETE from film WHERE title = 'Euclidean PI'--no because it's still being used in film_actor, and film_id
 
 -- 12. Check database metadata to determine all constraints of the film id, and
 -- describe any remaining adjustments needed before the film "Euclidean PI" can
 -- be removed from the film table.
+--CANNOT REMEMBER how to access metadata but it is being used in film_actor and film_id
